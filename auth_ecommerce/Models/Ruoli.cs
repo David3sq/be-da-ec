9ea20a1696b30ccc970b6cf5c31b4b6f12fbbf0f
squar <dvdsqrc@gmail.com>
2025-04-12 17:53:59 +0200

    added models role, for distict roles type

---
+++ auth_ecommerce/Models/Ruoli.cs
@@ -0,0 +1,10 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ecommerce.auth_ecommerce.Models;
+
+public class Ruoli
+{
+    [Key]
+    public int Id { get; set; }
+    public List<string> Roles { get; set; } = new List<string> {"Admin","Manager","User"};
+}
